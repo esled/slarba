38a4e37e6e430dbf71a1718039f11bc261fcec41
Jori Niskanen <joritius@luukku.com>
2015-03-26 18:10:07 +0200

    GPS addon

--- gps.py
+++ gps.py
@@ -2,11 +2,13 @@ import microstacknode.gps.l80gps, time
 
 gps = microstacknode.gps.l80gps.L80GPS()
 
-file = open('gps_data', 'a')
+database = open('gps_database', 'a')
+last_data = open('gps_data', 'w')
 while 1:
 	data = gps.gpgga
 	gps_data = str(data['utc']) + ";" + str(data['longitude']) + ";" + str(data['latitude']) + ";\n"
 	file.write(gps_data)
+	file.write(database)
 	time.sleep(0.49)
 	
 
