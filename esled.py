289e4997a5b322eb2e177ab56037bcdb24ab5d20
Royppeliini <Jori.Niskanen@bitparty.info>
2015-05-22 11:41:44 +0000

    Added SHT21 sensor

--- esled.py
+++ esled.py
@@ -1,8 +1,12 @@
-import os, time, sys, Adafruit_DHT
+import os, time, sys, sht21#, Adafruit_DHT
 from multiprocessing import Value, Array
 from subprocess import call
 
-NUM_PROCESSES = 7
+with sht21.SHT21(1) as sht21:
+	print "T: %s"%sht21.read_temperature()
+	print "H: %s"%sht21.read_humidity()
+
+NUM_PROCESSES = 6 #JOS DHT22 -> 7
 GPS_Data = Value('d',0.0)
 RTC_Data = Array('c',"kissa                          ")
 CAN_Data = Array('c',"Koira")
@@ -11,7 +15,8 @@ D1W_1 = Value('d',0.0)
 DHT_1_T = Value('d',0.0)
 DHT_1_H = Value('d',0.0)
 DHT_1_W = Value('i',5)
-
+SHT21_T = Value('d',0.0)
+SHT21_H = Value('d',0.0)
 
 kissa = Value('d', 1.0)
 kissat= Array('i', range(10))
@@ -47,18 +52,20 @@ def anturit():
 	contents2 = contents[-1].split("=")
 	contents3 = int(contents2[-1])
 	D1W_1.value = contents3/1000.
+	SHT21_T.value = sht21.read_temperature()
+	SHT21_H.value = sht21.read_humidity()
 
 def DHT():
-	if DHT_1_W.value > 4:
-		DHT_1_W.value = 0
-		humidity, temperature = Adafruit_DHT.read(Adafruit_DHT.DHT22, "17")
-		if humidity is not None and temperature is not None:
-			DHT_1_T.value = temperature
-			DHT_1_H.value = humidity
-		else:
+#	if DHT_1_W.value > 4:
+#		DHT_1_W.value = 0
+#		humidity, temperature = Adafruit_DHT.read(Adafruit_DHT.DHT22, "17")
+#		if humidity is not None and temperature is not None:
+#			DHT_1_T.value = temperature
+#			DHT_1_H.value = humidity
+#		else:
 			print 'Arvojen lukeminen anturilta DHT22 portissa GPIO17 epaonnistui!\n'
-	else:
-		DHT_1_W.value = DHT_1_W.value + 1
+#	else:
+#		DHT_1_W.value = DHT_1_W.value + 1
 
 
 while 1:	
@@ -76,6 +83,9 @@ while 1:
 	print "DHT anturi 1"
 	print u'Temp={0:0.1f}\u00b0C\nHumidity={1:0.1f}%\n'.format(DHT_1_T.value, DHT_1_H.value)
 
+	print "SHT Anturi"
+	print u'Temp: {0:0.1f}\u00b0C'.format(SHT21_T.value)
+	print "Hum : {0:0.1f}%".format(SHT21_H.value)
 
 	start_time = time.time()
 	for process in range(NUM_PROCESSES):
