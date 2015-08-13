37f9c953cf6ec7d8eef461cdacfe9b4c104ee9bc
esledrasp <antti.juntunen@edu.lapinamk.fi>
2015-08-13 14:35:38 +0300

    kkk

--- softa.py
+++ softa.py
@@ -74,9 +74,9 @@ class Example(AVC):
    def main(self):
 	#gtk.main()
 	while 1:
-		teksti = '<span size="40000"><b>%s</b></span>\n<span size="40000"><b>Koordit</b></span>\n<span size="40000"><b>CANintila</b></span>\n<span size="40000"><b>KISSA</b></span>\n<i><span size="40000"><b>KISSA</b></span></i>\n<span size="40000"><b>KISSA3</b></span>' % RTC_Data.value
+		teksti = '<span size="40000"><b>%s</b></span>\n<span size="40000"><b>Koddordit</b></span>\n<span size="40000"><b>CANintila</b></span>\n<span size="40000"><b>KISSA</b></span>\n<i><span size="40000"><b>KISSA</b></span></i>\n<span size="40000"><b>KISSA3</b></span>' % RTC_Data.value
 		label.set_markup(teksti)
-		gtk.main_iteration()
+		gtk.main()
 	
  #### MAIN
  
