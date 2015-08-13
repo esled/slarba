7762ac149aa46b89f6b63a48e984dab274e93f84
esledrasp <antti.juntunen@edu.lapinamk.fi>
2015-08-13 15:29:00 +0300

    wwewer

--- softa.py
+++ softa.py
@@ -69,7 +69,7 @@ class Example(AVC):
 	label2 = gtk.Label()
 	#label2.set_angle(190)
 	label2.set_name('testi')
-	label2.set_markup('<span size="40000">Kello:</span>\n<span size="40000">GPS:</span>\n<span size="40000">SHT21_H:</span>\n<span size="40000">SHT21_T:</span>\n<span size="40000">TEMP:</span>\n<span size="40000">HUM:</span>')
+	label2.set_markup('<span size="40000">Kello:</span>\n<span size="40000">GPS:</span>\n<span size="40000">SHT21_H:</span>\n<span size="40000">SHT21_T:</span>\n<span size="40000">TEMP:</span>\n<span size="40000">Accelerometer x,y,z:</span>')
 	col1.add(label2)
 
 
@@ -78,7 +78,7 @@ class Example(AVC):
    def loop(self):
 	#gtk.main()
 	while 1:
-		teksti = '<span size="40000"><b>%s</b></span>\n<span size="40000"><b>%f, %f</b></span>\n<span size="40000"><b>%.2f</b></span>\n<span size="40000"><b>%.2f</b></span>\n<i><span size="40000"><b>%.2f</b></span></i>\n<span size="40000"><b>%.4f, %.4f, %.4f</b></span>' % (RTC_Data.value, Longitude.value, Latitude.value,SHT21_H.value, SHT21_T.value, D1W_1.value, Axelerometer_x.value, Axelerometer_y.value, Axelerometer_z.value)
+		teksti = '<span size="40000"><b>%s</b></span>\n<span size="40000"><b>%f, %f</b></span>\n<span size="40000"><b>%.2f</b></span>\n<span size="40000"><b>%.2f</b></span>\n<i><span size="40000"><b>%.2f</b></span></i>\n<span size="40000"><b>%.1f, %.1f, %.1f</b></span>' % (RTC_Data.value, Longitude.value, Latitude.value,SHT21_H.value, SHT21_T.value, D1W_1.value, Axelerometer_x.value, Axelerometer_y.value, Axelerometer_z.value)
 		self.label.set_markup(teksti)
 		gtk.main_iteration()
 	
