6f71f7d24e23a534151a3dce4abd306dc96ef437
esledrasp <antti.juntunen@edu.lapinamk.fi>
2015-08-13 14:49:55 +0300

    a

--- softa.py
+++ softa.py
@@ -74,7 +74,7 @@ class Example(AVC):
    def loop(self):
 	#gtk.main()
 	while 1:
-		teksti = '<span size="40000"><b>%s</b></span>\n<span size="40000"><b>%d, %d</b></span>\n<span size="40000"><b>CANintila</b></span>\n<span size="40000"><b>KISSA</b></span>\n<i><span size="40000"><b>KISSA</b></span></i>\n<span size="40000"><b>KISSA3</b></span>' % RTC_Data.value, Longitude.value, Latitude.value
+		teksti = '<span size="40000"><b>%s</b></span>\n<span size="40000"><b>%d, %d</b></span>\n<span size="40000"><b>CANintila</b></span>\n<span size="40000"><b>KISSA</b></span>\n<i><span size="40000"><b>KISSA</b></span></i>\n<span size="40000"><b>KISSA3</b></span>' % (RTC_Data.value, Longitude.value, Latitude.value)
 		self.label.set_markup(teksti)
 		gtk.main_iteration()
 	
